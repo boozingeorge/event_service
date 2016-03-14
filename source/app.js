bcb5ffe8af10a8509cc35119c14b7e6599ce3fa6
George Boozin <boozingeorge@gmail.com>
2016-03-14 19:01:43 +0600

    added map component with custom marker

--- source/app.js
+++ source/app.js
@@ -1,1 +1,9 @@
 var EventService = angular.module('EventService',['ngMaterial']);
+function MainController($scope, $element, $attrs){
+
+}
+EventService.component('main', {
+  template:'<map></map>',
+  transclude: true,
+  controller:MainController
+});

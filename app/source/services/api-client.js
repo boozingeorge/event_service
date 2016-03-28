379e8d40ac954510b237eceff5b525d79ce0ab70
nnazarchuk <nik.nazarchuk@gmail.com>
2016-03-28 19:25:19 +0000

    T-21: Fix design theme, minor refactoring

---
+++ app/source/services/api-client.js
@@ -0,0 +1,53 @@
+EventService.factory('APIClient', APIClientService);
+
+function APIClientService($http) {
+  
+  function APIClient() {
+    if (!store.enabled) {
+      alert('Local storage is not supported by your browser. Please disable "Private Mode", or upgrade to a modern browser');
+      return;
+    }
+    store.clear();
+  }
+
+  APIClient.prototype.getToken = function (callback) {
+    var token = store.get('token');
+    var expiresIn = store.get('expires_in');
+    var timestamp = store.get('timestamp');
+
+    if (!token || !expiresIn || !timestamp) {
+      $http({
+        method: 'GET',
+        url: '/api/get_access_token'
+      }).then(function (response) {
+        store.set('token', response.data.token);
+        store.set('timestamp', (new Date().getTime()));
+        store.set('expires_in', response.data.expires_in);
+        callback(null, response.data.token);
+      }, function (xhr, status, error) {
+        //TO DO response code
+        console.log('/api/get_access_token -- failed');
+        var error = JSON.parse(xhr.responseText);
+        callback({
+          error: error.error || 'internal_server_error',
+          error_message: error.error_message || 'Internal server error'
+        });
+      });
+    } else {
+      var expiresInUpdated = expiresIn - Math.floor(((new Date().getTime()) - timestamp) / 1000);
+      /**
+       * if (expiresInUpdated < 0) refresh token if refresh_error
+       */
+      if (expiresInUpdated > 0) {
+        callback(null, token);
+      } else {
+        callback({
+          error: 'token_expired',
+          error_message: 'Token is expired'
+        });
+      }
+    }
+  };
+
+  return new APIClient();
+}

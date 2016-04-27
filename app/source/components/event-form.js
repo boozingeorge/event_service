cd6aeb92731b9afd8247789df128ecc143ab1afd
nnazarchuk <nik.nazarchuk@gmail.com>
2016-04-27 18:23:21 +0000

    T-30: Fix event form

--- app/source/components/event-form.js
+++ app/source/components/event-form.js
@@ -1,15 +1,16 @@
 function EventFormController(GoogleMap, $timeout, $scope, PopUp, APIClient, EventDatetime) {
-
   var ctrl = this;
 
   initEventForm();
-
+  
   ctrl.setLocation = function () {
+    ctrl.locationDisabled = true;
     var locationOnClick = google.maps.event.addListener(GoogleMap.map, 'click', function (event) {
       $timeout(function () {
         ctrl.event.location.lat = event.latLng.lat();
         ctrl.event.location.long = event.latLng.lng();
         ctrl.viewLocation = event.latLng.lat().toFixed(4) + ', ' + event.latLng.lng().toFixed(4);
+        ctrl.locationDisabled = false;
       });
       locationOnClick.remove();
     });
@@ -21,6 +22,7 @@ function EventFormController(GoogleMap, $timeout, $scope, PopUp, APIClient, Even
     if (!ctrl.event.picture) {
       delete ctrl.event['picture'];
     }
+    
     APIClient.createEvent(ctrl.event).then(function (response) {
       if (response.data.error) {
         throw new Error(response.data.error.error_message);
@@ -34,7 +36,7 @@ function EventFormController(GoogleMap, $timeout, $scope, PopUp, APIClient, Even
       PopUp.Error();
     });
   };
-
+  
   function initEventForm() {
     ctrl.event = {
       title: '',
@@ -54,6 +56,16 @@ function EventFormController(GoogleMap, $timeout, $scope, PopUp, APIClient, Even
     ctrl.beginAt = new EventDatetime(new Date(), nowDate, maxDate);
     ctrl.endAt = new EventDatetime(endDate, nowDate, maxDate);
     ctrl.viewLocation = '';
+    ctrl.locationDisabled = false;
+    if (ctrl.eventForm) {
+      ctrl.eventForm.$setPristine();
+      ctrl.eventForm.$setUntouched();
+      // Reset description input char counter to 0/<max_chars>
+      var descriptionCounterEl = angular.element(document.getElementsByClassName('md-char-counter'));
+      var counterVal = descriptionCounterEl.html().split('/');
+      counterVal[0] = '0';
+      descriptionCounterEl.html(counterVal.join('/'));
+    }
   }
 }
 
@@ -77,12 +89,32 @@ EventService.directive("validDatetime", function () {
     link: function ($scope, element, attributes) {
       $scope.changeDatetimeHandler = function () {
         if ($scope.beginAt.getDatetime() >= $scope.endAt.getDatetime()) {
-          $scope.form.Datetime.$setTouched();
-          $scope.form.Datetime.$setValidity('Datetime', false);
+          $scope.form.datetime.$setTouched();
+          $scope.form.datetime.$setValidity('datetime', false);
         } else {
-          $scope.form.Datetime.$setValidity('Datetime', true);
+          $scope.form.datetime.$setValidity('datetime', true);
         }
       }
+    }
+  };
+});
+
+EventService.directive("validPictureUpload", function () {
+  return {
+    restrict: "A",
+    scope: {
+      changeUrlHandler: '=',
+      form: '=',
+      picture: '='
+    },
+    link: function ($scope, element, attributes) {
+      $scope.changeUrlHandler = function (e) {
+        if ($scope.picture && !$scope.picture.match(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/ig)) {
+          $scope.form.picture.$setValidity('url', false);
+        } else {
+          $scope.form.picture.$setValidity('url', true);
+        }
+      };
     }
   };
 });
